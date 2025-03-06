2c791255e3828ed9f70c99d1f131c386b8903004
Arghya Paul <arghyapaul159@gmail.com>
2025-03-06 11:24:59 +0530

    Updated NoMatchException.java
    Added Comments

--- NoMatchException.java
+++ NoMatchException.java
@@ -1,7 +1,11 @@
 package Part3;
 
+// Custom exception class that extends the built-in Exception class
 public class NoMatchException extends Exception {
+    
+    // Constructor that accepts line number and input string as parameters
     NoMatchException(int line, String input) {
-        super("Exception at line number: " + line + " No Match Found for "+ input);
+        // Calls the superclass constructor with a custom exception message
+        super("Exception at line number: " + line + " No Match Found for " + input);
     }
 }
